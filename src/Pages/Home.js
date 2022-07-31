c1c0f750173daa784b3b2e56aad8556878a67a31
Ramalingam <ramalingamveerabahu@gmail.com>
2022-07-31 12:24:07 +0530

    created input and fetch results from API

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageComponent from '../components/MainPageComponent';
 
 const Home = () => {
-  return <MainPageComponent>This is home....</MainPageComponent>;
+  const [input, setInput] = useState('');
+
+  const keyPress = ev => {
+    setInput(ev.target.value);
+  };
+
+  const search = () => {
+    // https://api.tvmaze.com/search/shows?q=girls
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(r => r.json())
+      .then(result => {
+        console.log(result);
+      });
+  };
+
+  const keyDown = ev => {
+    if (ev.keyCode === 13) {
+      search();
+    }
+  };
+
+  return (
+    <MainPageComponent>
+      <input
+        type="text"
+        onChange={keyPress}
+        onKeyDown={keyDown}
+        value={input}
+      ></input>
+      <input type="button" onClick={search} value="Search"></input>
+    </MainPageComponent>
+  );
 };
 
 export default Home;
